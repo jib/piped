69351e1e858cd3762cd75661a80196608487c538
Jos Boumans <jos@dwim.org>
2012-01-12 13:52:30 -0800

    * move some defaults here

--- etc/config.js
+++ etc/config.js
@@ -1,4 +1,8 @@
-{
-  trace: true,
-  foo: 42,
+// TODO: come up with defaults & documentation here
+{   debug:  true,
+    trace:  true,
+    servers: [
+        [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
+        [ "tcp://localhost:10002", "udp://localhost:10005" ],
+    ],
 };
