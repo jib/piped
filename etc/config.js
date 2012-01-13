2b9bd311508cd5b2870b08088b8d1a9e4df7481b
Jos Boumans <jos@dwim.org>
2012-01-13 15:32:10 -0800

    * Add a State.current_servers list which is used by the listeners   to dispatch data * List is updated periodically every Config.rescan_interval ms * Overflow log is added automatically if any of the delivery   chains has no target, and removed when all chains are healthy   again. * Add 'active servers' to stats admin command * Robustify the admin commands so they can never take down the   server on exception * Rename State.servers to State.all_servers for clarity

--- etc/config.js
+++ etc/config.js
@@ -1,6 +1,7 @@
 // TODO: come up with defaults & documentation here
 {   debug:  true,
     trace:  true,
+    tcp_port: 1337,
     servers: [
         [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
         [ "tcp://localhost:10002", "udp://localhost:10005" ],
