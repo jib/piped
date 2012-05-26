4a7da2e0177e3fd98a133051f115ee9e172e17cb
Jos Boumans <jos@dwim.org>
2012-05-26 00:34:39 -0700

    * simplified config

--- etc/config.js
+++ etc/config.js
@@ -1,9 +1,12 @@
 // TODO: come up with defaults & documentation here
 {   debug:  true,
-    trace:  true,
+    trace:  false,
     tcp_port: 1337,
     servers: [
-        [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
-        [ "tcp://localhost:10002", "udp://localhost:10005" ],
+//         [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
+//         [ "tcp://localhost:10002", "udp://localhost:10005" ],
+        [ "tcp://localhost:10001" ],
+        [ "tcp://localhost:10002" ],
+
     ],
 };
