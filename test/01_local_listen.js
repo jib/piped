6f3b09e2d23d5c5e602c288236fe917d36148f70
Jos Boumans <jos@dwim.org>
2012-05-28 10:42:35 -0700

    * add support for udp listening + tests

--- test/01_local_listen.js
+++ test/01_local_listen.js
@@ -3,20 +3,72 @@ var RS              = require('../lib/remote_send');
 var Configurator    = require('../lib/configurator');
 var Base            = require('../lib//base');
 var C               = require('../lib/common');
+
+var Dgram           = require("dgram");
 var U               = require('util');
 var Net             = require('net');
 
 
 var BO = new Base.BaseObject();
 
+var _stream_test = function( type, port, host ) {
+
+    // The listener
+    var ll = new LL.LocalStreamListen( type, port, host,
+        // on connect
+        function( ll, conn ) {
+            C._trace( ll );
+
+            conn.on( 'data', function (data) {
+                C._trace( [ U.format( "OK: %s:%s", type, port), data ] );
+                OK++;
+            });
+        },
+        // on listen
+        function( ll ) {
+            var sock = Net.createConnection( port, host );
+
+            C._trace( [ "socket: ", sock  ]);
+
+            sock.on( 'error', function(e) { C._trace( [ type, e ] ); FAIL++ } );
+            sock.write( type );
+        }
+    );
+};
+
+var _udp_test = function( type, port, host ) {
+
+    // The listener
+    var ll = new LL.LocalUDPListen( type, port, host,
+        // on data
+        function( ll, data ) {
+            C._trace( ll );
+            C._trace( [ U.format( "OK: %s:%s", type, port), data ] );
+            OK++;
+        },
+        // on listen
+        function( ll ) {
+            var sock = Dgram.createSocket("udp4");
+
+            C._trace( [ "udp: ", sock  ]);
+            sock.on( 'error', function(e) { C._trace( [ type, e ] ); FAIL++ } );
+
+            var buf  = new Buffer( type );
+            sock.send( buf, 0, buf.length, port, host );
+        }
+    );
+};
+
 var TESTS       = {
-    'tcp':  [ 10001, 'localhost' ],
-    'unix': [ '/tmp/socket.piped' ],
+    'unix': [ _stream_test, '/tmp/socket.piped' ],
+    'tcp':  [ _stream_test, 10001, 'localhost' ],
+    'udp':  [ _udp_test,    10011, 'localhost' ],
 }
 
-var TestCount   = 2;    // XXX count the keys of TESTS
+var TestCount   = 3;    // XXX count the keys of TESTS
 var OK          = 0;
 var FAIL        = 0;
+
 
 Configurator.config( null, ["--debug", "--trace"], function( config_object ) {
     BO._set_config_object( config_object );
@@ -27,7 +79,7 @@ Configurator.config( null, ["--debug", "--trace"], function( config_object ) {
         C._trace( [ type, TESTS[type] ] );
 
         // Run the individual test
-        _test( type, TESTS[type][0], TESTS[type][1] );
+        TESTS[type][0]( type, TESTS[type][1], TESTS[type][2] );
     }
 });
 
@@ -46,27 +98,4 @@ setInterval( function () {
 }, 1000 );
 
 
-function _test( type, port, host ) {
 
-    // The listener
-    var ll = new LL.LocalStreamListen( type, port, host,
-        // on connect
-        function( ll, conn ) {
-            C._trace( ll );
-
-            conn.on( 'data', function (data) {
-                C._trace( [ "tcp:10001", data ] );
-                OK++;
-            });
-        },
-        // on listen
-        function( ll ) {
-            var sock = Net.createConnection( port, host );
-
-            C._trace( [ "socket: ", sock  ]);
-
-            sock.on( 'error', function(e) { C._trace( [ type, e ] ); FAIL++ } );
-            sock.write( type );
-        }
-    );
-};
