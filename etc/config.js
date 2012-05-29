adba539486cc2a3e9191a427ab6a2df5c2d7116c
Jos Boumans <jos@dwim.org>
2012-05-29 14:51:53 -0400

    * tweaking

--- etc/config.js
+++ etc/config.js
@@ -2,11 +2,12 @@
 {   debug:  true,
     trace:  false,
     tcp_port: 1337,
+    udp_port: 1337,
     servers: [
 //         [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
 //         [ "tcp://localhost:10002", "udp://localhost:10005" ],
         [ "tcp://localhost:10001" ],
-        [ "tcp://localhost:10002" ],
-
+        //[ "tcp://localhost:10002" ],
+        [ "udp://localhost:10003" ],
     ],
 };
