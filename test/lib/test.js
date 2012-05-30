89405e9fc28ac4918f6c0ad2af24f8bb88d33c37
Jos Boumans <jos@dwim.org>
2012-05-29 20:23:09 -0400

    * support no_plan

--- test/lib/test.js
+++ test/lib/test.js
@@ -16,11 +16,18 @@ var Test = exports.Test = function( test_count, func, time_out ) {
         obj.ok          = 0;
         obj.fail        = 0;
         obj.checks      = 0;
-        obj.time_out    = time_out || 5;
-        obj.test_count  = test_count;
+        obj.time_out    = time_out   || 5;
+        obj.test_count  = test_count || 'no_plan';
+        obj.no_plan     = obj.test_count == 'no_plan' ? "UNKNOWN" : false;
+
+        if( obj.no_plan ) {
+            C._log( 'You are using "no_plan" - that is good for development, but ' +
+                    'you will want to switch to a test count before commiting: '   +
+                    'now you will have to wait until the test timeout' );
+        }
 
         C._log( U.format( "TEST: Expecting %s tests (timeout: %s secs)",
-            obj.test_count, obj.time_out ) );
+            obj.no_plan || obj.test_count, obj.time_out ) );
 
         // **************************
         // Check if tests succeeded
@@ -41,13 +48,15 @@ var Test = exports.Test = function( test_count, func, time_out ) {
                 if( timed_out ) {
                     U.log( U.format(
                         "Execution time (%s sec) expired. Missing %s test results",
-                        obj.time_out, obj.test_count - obj.ok - obj.fail
+                        obj.time_out, obj.no_plan || (obj.test_count - obj.ok - obj.fail)
                     ) );
                 }
 
                 // exit with the amount of failed tests, or 255 if we exceeded
-                // the time out.
-                var my_exit = timed_out ? 255      :
+                // the time out. Unless you were using 'no_plan', and we will
+                // just exit with the failure count, which may have been 0,
+                // at which point the run succeeded
+                var my_exit = timed_out ? (obj.no_plan ? obj.fail : 255  ) :
                               obj.fail  ? obj.fail :
                               0;
 
