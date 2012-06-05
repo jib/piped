4ae95c09d6d9fff1e622b461086e3fd6493d4390
Jos Boumans <jos@dwim.org>
2012-06-06 00:45:34 +0200

    * updated config

--- etc/config.js
+++ etc/config.js
@@ -3,11 +3,13 @@
     trace:  false,
     tcp_port: 1337,
     udp_port: 1337,
+    unix_socket: '/tmp/piped.socket',
     servers: [
 //         [ "tcp://localhost:10001", "socket:///tmp/echo1.socket" ],
 //         [ "tcp://localhost:10002", "udp://localhost:10005" ],
 //        [ "tcp://localhost:10001" ],
-        //[ "tcp://localhost:10002" ],
-        [ "udp://localhost:10003" ],
+        [ "tcp://localhost:10001" ],
+        [ "udp://localhost:10001" ],
+        [ "socket:///tmp/remote.socket" ],
     ],
 };
