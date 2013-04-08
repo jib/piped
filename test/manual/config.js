5f3dc33c1edf41e17277d9eef13644e67a0e7047
Jos Boumans <jos@dwim.org>
2013-04-08 16:22:06 -0700

    * make sure unix sockets dont break host/port parsing

--- test/manual/config.js
+++ test/manual/config.js
@@ -6,7 +6,7 @@
     unix_socket: '/tmp/piped.socket',
     servers: [
         [ "tcp://localhost:10001" ],
-        [ "udp://localhost:10001" ],
-        [ "socket:///tmp/remote.socket" ],
+        //[ "udp://localhost:10001" ],
+        //[ "socket:///tmp/remote.socket" ],
     ],
 };
